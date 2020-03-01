f4ebd8740eedb9e204d1a3e861dc97a5eff9692e
kdator <colpakov.dmitry@gmail.com>
2020-03-01 23:00:00 +0300

    fixed files with reverted string.

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,9 @@
-#include "revert_string.h"
 #include <string.h>
 
+#include "revert_string.h"
+
 void RevertString(char *str) {
+
   for (int i = 0; i < (strlen(str) / 2) + 1; i++) {
 
     char temp = str[i];
