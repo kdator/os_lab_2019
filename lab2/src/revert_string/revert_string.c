a3639d749a5a07be5f03273ed331d71885281aa0
kdator <colpakov.dmitry@gmail.com>
2020-03-09 03:43:10 +0300

    fix revert

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -4,7 +4,7 @@
 
 void RevertString(char *str) {
 
-  for (int i = 0; i < (strlen(str) / 2) + 1; i++) {
+  for (int i = 0; i < (strlen(str) / 2); i++) {
 
     char temp = str[i];
     str[i] = str[(strlen(str) -1 - i)];
