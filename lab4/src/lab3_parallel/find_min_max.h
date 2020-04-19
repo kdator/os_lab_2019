bab87cd35be3e75e849ab265271e5757924dfd5e
kdator <colpakov.dmitry@gmail.com>
2020-04-19 14:33:29 +0300

    add: --timeout to parallel_min_max.c

---
+++ lab4/src/lab3_parallel/find_min_max.h
@@ -0,0 +1,11 @@
+#ifndef FIND_MIN_MAX_H
+#define FIND_MIN_MAX_H
+
+#include "utils.h"
+
+/*
+ *  This function implemets search a max and a min element in an array.
+ */
+struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end);
+
+#endif
